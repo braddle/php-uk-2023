addeda01e7f0f166d5b9c2aa879644ec1fb45e40
Mark Bradley <braddle@gmail.com>
2023-02-14 10:18:10 +0000

    Stack Demo

--- src/ADT/Stack.php
+++ src/ADT/Stack.php
@@ -5,4 +5,25 @@ namespace Braddle\PhpUk2023\ADT;
 class Stack
 {
 
+    private $count = 0;
+    private $values = [];
+    public function isEmpty() :bool
+    {
+        return !$this->count;
+    }
+
+    public function push(string $value) : void
+    {
+        $this->values[$this->count++] = $value;
+    }
+
+    public function count() : int
+    {
+        return $this->count;
+    }
+
+    public function pop() : string
+    {
+        return $this->values[--$this->count];
+    }
 }
--- test/ADT/StackTest.php
+++ test/ADT/StackTest.php
@@ -2,13 +2,66 @@
 
 namespace ADT;
 
+use Braddle\PhpUk2023\ADT\Stack;
 use PHPUnit\Framework\TestCase;
 use SebastianBergmann\FileIterator\Facade;
 
 class StackTest extends TestCase
 {
-    public function testHolding()
+
+    private $empty, $one, $two;
+
+    protected function setUp() : void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        $this->empty = new Stack();
+
+        $this->one = new Stack();
+        $this->one->push("snap");
+
+        $this->two = new Stack();
+        $this->two->push("snap");
+        $this->two->push("crackle");
+
     }
+
+
+    public function testEmpty()
+    {
+        $this->assertTrue($this->empty->isEmpty());
+    }
+
+    public function testNotEmpty()
+    {
+        $this->assertFalse($this->one->isEmpty());
+    }
+
+    public function testEmptyIsZero()
+    {
+        $this->assertEquals(0, $this->empty->count());
+    }
+
+    public function testCountOfOne()
+    {
+        $this->assertEquals(1, $this->one->count());
+    }
+
+    public function testCountOfTwo()
+    {
+        $this->assertEquals(2, $this->two->count());
+    }
+
+    public function testPopOffOne()
+    {
+        $this->assertEquals("snap", $this->one->pop());
+    }
+
+    public function testPopOffTwo()
+    {
+        $this->assertEquals("crackle", $this->two->pop());
+        $this->assertEquals("snap", $this->two->pop());
+    }
+
+
 }
